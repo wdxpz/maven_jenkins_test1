65cbb9400d33fc854e7ecc5ca52afc27c79dca0f
unknown <>
2016-05-03 16:18:28 +0800

    first init reposite

---
+++ src/test/java/com/sw/mvntest/AppTest.java
@@ -0,0 +1,19 @@
+package com.sw.mvntest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+   
+    @Test
+    public void test2(){
+    	
+    }
+}
